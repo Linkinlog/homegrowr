628d75b977f7f9dc173eff7b967950f803d892fe
linkinlog <dahlton@dahlton.org>
2022-04-03 19:44:46 -0400

    WIP - added skelteton for api expansion

--- routes/api/apiRoutes.php
+++ routes/api/apiRoutes.php
@@ -5,12 +5,16 @@ use \App\Models\Sensors;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-//Route for adding readings
-Route::post('/addreading', function () {
+
+//General Scaffolding for CRUD REST API
+
+
+//* Create {reading}
+
+Route::post('/reading', function () {
     if (!isset($_REQUEST['value']) or !isset($_REQUEST['type']) or !isset($_REQUEST['uuid'])) {
         return;
     }
-    // return $_REQUEST;
     $uuid = $_REQUEST['uuid'];
     $type = $_REQUEST['type'];
     $plant_id = isset($_REQUEST['plant_id']) ? $_REQUEST['plant_id'] : 0;
@@ -29,40 +33,37 @@ Route::post('/addreading', function () {
     return $result;
 });
 
+
+//* Create {sensor}
+
+//* Create {plant}
+
+
+
+//* Read {reading}
+
 //Route for getting base amount of readings - 15 by default
 Route::get('/', function () {
     return Reading::getReadingsByTS(15);
 });
 
-//Route for getting specified amount of readings
-Route::get('readings/{limit}', function ($limit) {
+Route::get('/readings', function () {
+    if (isset($_REQUEST['limit'])) {
+        $limit = $_REQUEST['limit'];
+    } else {
+        $limit = 10;
+    }
     return Reading::getReadingsByTS($limit);
 });
 
 //Route for getting readings specifically for atmosphere
-Route::get('/atmosphere', function () {
+Route::get('/readings/atmosphere', function () {
     return Reading::atmosphere();
 });
 
-// Query routes for checking status of relays
-Route::get('/q/{uuid}', function ($uuid) {
-    $id = Sensors::where('uuid', $uuid)->first()->value('relay_pin');
-    return exec("sudo /usr/bin/python3 /opt/scripts/relay-switcher.py $id query");
-});
 
-// Check routes for seeing if the fans should be on/off
-Route::get('/check', function () {
-    $sensors = Sensors::select('alias', 'uuid')
-        ->groupBy('uuid')
-        ->get();
-    echo '<ul>';
-    foreach ($sensors as $sensor) {
-        echo "<li><a style='font-size:100px; padding: 7px;' href='/api/check/$sensor->uuid'>$sensor->alias</a></li>";
-    }
-    echo '</ul>';
-});
-
-Route::get('/check/{uuid}', function ($uuid) {
+//Get readings for given sensor uuid
+Route::get('/readings/{uuid}', function ($uuid) {
     //TODO Reading::checkStats($uuid);
     // $readings = new Reading;
     // $readings->uuid = $uuid;
@@ -95,7 +96,48 @@ Route::get('/check/{uuid}', function ($uuid) {
     }
 });
 
-//UUID Routes
-Route::get('/uuid', function () {
+
+//* Read {relay_pin}
+
+// Query routes for checking status of relays
+Route::get('/relay_pin/{uuid}', function ($uuid) {
+    $id = Sensors::where('uuid', $uuid)->first()->value('relay_pin');
+    return exec("sudo /usr/bin/python3 /opt/scripts/relay-switcher.py $id query");
+});
+
+//* Read {sensors}
+
+// Check routes for seeing if the fans should be on/off
+Route::get('/sensors', function () {
+    $sensors = Sensors::select('alias', 'uuid')
+        ->groupBy('uuid')
+        ->get();
+    echo '<ul>';
+    foreach ($sensors as $sensor) {
+        echo "<li><a style='font-size:100px; padding: 7px;' href='/api/readings/$sensor->uuid'>$sensor->alias</a></li>";
+    }
+    echo '</ul>';
+});
+
+//Query all UUIDs
+Route::get('/sensors/uuids', function () {
     return Sensors::getUUIDs();
 });
+
+//* Read {plant}
+
+
+
+//* Update {reading}
+
+//* Update {sensor}
+
+//* Update {plant}
+
+
+
+//* Delete {reading}
+
+//* Delete {sensor}
+
+//* Delete {plant}
