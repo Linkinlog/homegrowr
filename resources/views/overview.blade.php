1b9611e5b9fc3ee97e58404ed81f9c8c12c29af0
linkinlog <dahlton@dahlton.org>
2022-04-21 05:58:36 -0400

    WIP - added cameras

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -1,13 +1,26 @@
 @extends('layouts.layout')
 
 @section('content')
-<div id="app">
+<div id="app" style="padding-bottom: 20px;">
     <div class="container pt-3">
         <h1 class="text-center">HomeGrowR Overview</h1>
         <div class="row">
-            @for ($i = 0; $i < count($data); ++$i)
+            @for ($i = 0; $i < count($atmosphere); ++$i)
                 <div class="col-md-6">
-                    <chart name={{ $data[$i]['alias'] }} uuid={{ $data[$i]['uuid'] }} ></chart>
+                    <chart name={{ $atmosphere[$i]['alias'] }} uuid={{ $atmosphere[$i]['uuid'] }} ></chart>
+                </div>
+            @endfor
+            @for ($i = 0; $i < count($cameras); ++$i)
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header text-center">{{ $cameras[$i]['alias'] }}</div>
+                        <div class="card-body">
+                            <img width="100%" height="100%" src="/test" alt="webcam"></img>
+                            <video control type="video/mpeg4">
+                                <source src="/videos/?ip={{ urlencode($cameras[$i]['ipaddr']) }}">
+                            </video>
+                        </div>
+                    </div>
                 </div>
             @endfor
         </div>
--- routes/landing/landingRoutes.php
+++ routes/landing/landingRoutes.php
@@ -4,10 +4,28 @@ use Illuminate\Support\Facades\Route;
 use \App\Models\Sensors;
 
 Route::get('/overview', function () {
-    $data = Sensors::getUUIDsAndName();
-    return view('overview', ['data' => $data->toArray()]);
+    $atmosphere = Sensors::getUUIDsAndName('atmosphere');
+    $cameras = Sensors::getUUIDsAndName('camera');
+    return view('overview', ['atmosphere' => $atmosphere->toArray(), 'cameras' => $cameras->toArray()]);
 });
 
+
+Route::get('/videos/{$ip}', function ($ip) {
+    $server = $ip;
+    $url = "/mjpeg/1";
+    set_time_limit(0);
+    $fp = fsockopen($server, 80, $errno, $errstr, 30);
+    if (!$fp) {
+            echo "$errstr ($errno)<br>\n";
+    } else {
+            $urlstring = "GET ".$url." HTTP/1.0\r\n\r\n";
+            fputs ($fp, $urlstring);
+            while ($str = trim(fgets($fp, 4096)))
+            header($str);
+            fpassthru($fp);
+            fclose($fp);
+    }
+});
 
 Route::get('/', function () {
     return redirect('/overview');
